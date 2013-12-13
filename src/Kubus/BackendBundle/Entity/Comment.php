05de3f77e9e1bbbd521c956959acf7e1b60f646a
Ronald Klaus <ronald.klaus@gmx.net>
2013-12-13 18:49:58 +0100

    Add Comment entity and CRUD.

---
+++ src/Kubus/BackendBundle/Entity/Comment.php
@@ -0,0 +1,167 @@
+<?php
+
+namespace Kubus\BackendBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Comment
+ */
+class Comment
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var integer
+     */
+    private $messageId;
+
+    /**
+     * @var string
+     */
+    private $comment;
+
+    /**
+     * @var integer
+     */
+    private $personId;
+
+    /**
+     * @var \DateTime
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     */
+    private $updatedAt;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set messageId
+     *
+     * @param integer $messageId
+     * @return Comment
+     */
+    public function setMessageId($messageId)
+    {
+        $this->messageId = $messageId;
+    
+        return $this;
+    }
+
+    /**
+     * Get messageId
+     *
+     * @return integer 
+     */
+    public function getMessageId()
+    {
+        return $this->messageId;
+    }
+
+    /**
+     * Set comment
+     *
+     * @param string $comment
+     * @return Comment
+     */
+    public function setComment($comment)
+    {
+        $this->comment = $comment;
+    
+        return $this;
+    }
+
+    /**
+     * Get comment
+     *
+     * @return string 
+     */
+    public function getComment()
+    {
+        return $this->comment;
+    }
+
+    /**
+     * Set personId
+     *
+     * @param integer $personId
+     * @return Comment
+     */
+    public function setPersonId($personId)
+    {
+        $this->personId = $personId;
+    
+        return $this;
+    }
+
+    /**
+     * Get personId
+     *
+     * @return integer 
+     */
+    public function getPersonId()
+    {
+        return $this->personId;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return Comment
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     * @return Comment
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+    
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime 
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+}
---
+++ src/Kubus/BackendBundle/Form/CommentType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Kubus\BackendBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class CommentType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('messageId')
+            ->add('comment')
+            ->add('personId')
+            ->add('createdAt')
+            ->add('updatedAt')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Kubus\BackendBundle\Entity\Comment'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'kubus_backendbundle_comment';
+    }
+}
