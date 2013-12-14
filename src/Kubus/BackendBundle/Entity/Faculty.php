242f8eeef447f7526d131caa2096297dcd1ea060
Ronald Klaus <ronald.klaus@gmx.net>
2013-12-14 14:07:01 +0100

    Add Faculty entity and CRUD.

---
+++ src/Kubus/BackendBundle/Entity/Faculty.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace Kubus\BackendBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Faculty
+ */
+class Faculty
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $label;
+
+    /**
+     * @var integer
+     */
+    private $universityId;
+
+    /**
+     * @var \DateTime
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     */
+    private $updatedAt;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set label
+     *
+     * @param string $label
+     * @return Faculty
+     */
+    public function setLabel($label)
+    {
+        $this->label = $label;
+    
+        return $this;
+    }
+
+    /**
+     * Get label
+     *
+     * @return string 
+     */
+    public function getLabel()
+    {
+        return $this->label;
+    }
+
+    /**
+     * Set universityId
+     *
+     * @param integer $universityId
+     * @return Faculty
+     */
+    public function setUniversityId($universityId)
+    {
+        $this->universityId = $universityId;
+    
+        return $this;
+    }
+
+    /**
+     * Get universityId
+     *
+     * @return integer 
+     */
+    public function getUniversityId()
+    {
+        return $this->universityId;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return Faculty
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     * @return Faculty
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+    
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime 
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+}
